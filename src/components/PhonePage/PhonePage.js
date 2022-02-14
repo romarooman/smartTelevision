03364f051a5402b6fa23a397d9f308bcf4fcde08
Roman <romarooman@gmail.com>
2022-02-14 23:35:58 +0300

    remove redundant code

--- src/components/PhonePage/PhonePage.js
+++ src/components/PhonePage/PhonePage.js
@@ -1,21 +1,3 @@
-// import "./PhonePage.css";
-
-// let listNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// const PhonePage = () => {
-//   let numbers = [];
-//   listNumber.forEach((element) => {
-
-//     numbers.push(<div>{element}</div>);
-//     return numbers
-//   });
-//   return (
-//     <>
-//       <div className="phone">{numbers}</div>
-//     </>
-//   );
-// };
-// export { PhonePage };
-
 import React, { useState, useRef, useEffect } from "react";
 import "./PhonePage.css";
 import InputMask from "react-input-mask";
